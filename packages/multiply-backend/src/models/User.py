28dd53b474ded8542eff7501a932b3fd1bee977d
fall-development-rob <fall.development@gmail.com>
2024-11-11 16:41:47 +0000

    feat(api): expand the user model for frontend

--- packages/multiply-backend/src/models/User.py
+++ packages/multiply-backend/src/models/User.py
@@ -1,10 +1,15 @@
 from pydantic import Field, BaseModel
 import uuid
+from datetime import date
 
 class User(BaseModel):
     user_id: str = Field(default_factory=lambda: str(uuid.uuid4()), alias='userId')
     first_name: str = Field(..., alias='firstName')
+    last_name: str = Field(..., alias='lastName')
     email: str = Field(...)
+    age: int = Field(...)
+    birth_date: date = Field(..., alias='birthDate')
+    has_significant_other: bool = Field(..., alias='hasSignificantOther')
 
     class Config:
         populate_by_name = True
