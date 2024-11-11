d20fb90a4b24e5215c0a8f45368fcb9c4fbe3577
fall-development-rob <fall.development@gmail.com>
2024-11-11 13:59:44 +0000

    feat(api): add get route to user

--- packages/multiply-backend/src/routes/UserRoutes.py
+++ packages/multiply-backend/src/routes/UserRoutes.py
@@ -1,14 +1,16 @@
+import logging
+
 from flask import Blueprint, jsonify, request
 
 from src.models.User import User
 
 from src.services.UserService import UserService
 
-import logging
-
 logger = logging.getLogger(__name__)
 
 userRoutes = Blueprint('userRoutes', __name__, url_prefix='/user')
+
+userService = UserService()
 
 @userRoutes.route('', methods=['POST'])
 def createUser():
@@ -16,10 +18,17 @@ def createUser():
         data = request.get_json()
         userData = User(**data)
 
-        userService = UserService()
-
         user = userService.createUser(userData)
         return jsonify({'userId': user.user_id}), 201
     except Exception as e:
         logger.exception("Unhandled exception in createUser.")
+        return jsonify({'error': 'An internal error occurred.'}), 500
+    
+@userRoutes.route('/<userId>', methods=['GET'])
+def findUser(userId):
+    try:
+        user = userService.getUser(userId)
+        return jsonify(user.model_dump(by_alias=True)), 200
+    except Exception as e:
+        logger.exception("Unhandled exception in findUser.")
         return jsonify({'error': 'An internal error occurred.'}), 500
